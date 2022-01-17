d2d0f55bd4b9d64de6458f6875e7b3320adec8d7
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-17 23:37:17 +0530

    Constants, Variables, Keywords

---
+++ 002_Constants_Variables_Keywords.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	// int => keyword(also know as 'Reserved' words)
+	// var => variable(also know as 'Identifiers' and it can be changed)
+	// 10 => constant(also know as 'Literals' and it can't be changed)
+	int var = 10;
+	return 0;
+}
