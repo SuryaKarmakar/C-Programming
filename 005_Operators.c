ac83738f6d513a96422a5cfc18b60d9c6a700aec
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-19 23:01:01 +0530

    operators

---
+++ 005_Operators.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(){
+	//Arithmetic Operators :
+	int a = 20;           
+	int b = 10;
+	int add = a + b; // Addition
+	int sub = a - b; // Subtraction
+	int mul = a * b; // Multiplication
+	float div = a / b; // Division
+	int mod = a % b; // Modulus
+
+	//Increment & Decrement Operators :
+	// a++;
+	// b--;
+
+	// Relational Operators :
+	// a == b;		Is equal to
+	// a != b;		Is not equal to
+	// a > b;		Greater than
+	// a < b;		Less than
+	// a >= b;		Greater than or equal to
+	// a <= b;		Less than or equal to
+
+	// Logical Operators :
+	// a && b;		And operator
+	// a || b;		Or operator
+	// a ! b;		Not operator
+
+	// Bitwise Operators :
+	// <<			Binary Left Shift Operator
+	// >>			Binary Right Shift Operator
+	// ~			Binary Ones Complement Operator
+	// &			Binary AND Operator
+	// ^			Binary XOR Operator
+	// |			Binary OR Operator
+
+	// Assignment Operators :
+	// int var;
+	// var = 10;	Assign
+	// var += 1;	Increments then assign
+	// var -= 1;	Decrements then assign
+	// var *= 2;	Multiplies then assign
+	// var /= 2;	Divides then assign
+	// %=			Modulus then assign
+	// <<=			Left shift and assign
+	// >>=			Right shift and assign
+	// &=			Bitwise AND assign
+	// ^=			Bitwise exclusive OR and assign
+	// |=			Bitwise inclusive OR and assign
+	
+	// Conditional Operator : 
+	// ? :          Conditional Expression
+
+	// Special Operators : 
+	// sizeof()	    Returns the size of a memory location.
+    // &            Returns the address of a memory location.
+    // *	        Pointer to a variable.
+	return 0;
+}
