738ce6bb7da913d38f03e8d45abe0dc2578b9845
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-22 22:24:20 +0530

    using pow function

---
+++ 008_Pow_Function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// we need to include the math.h header file to using this pow() function.
+#include<math.h>
+
+int main(){
+	int a;
+	// pow(x,y) => x to the power y
+	a=pow(3,5);
+	printf("3 to the power 5 is : %d \n",a);
+	return 0;
+}
