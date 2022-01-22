2678bc858fc331067a289b074def99ffe66194af
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-22 22:30:25 +0530

    using sizeof function

---
+++ 009_Sizeof_Function.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+	int i;
+	//sizeof() is used with the data types such as int, float, char… etc it simply returns the amount of memory is allocated to that data types.
+	printf("int is %d bit \n",sizeof(i));
+	return 0;
+}
