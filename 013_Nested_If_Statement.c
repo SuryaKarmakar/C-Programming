c3de87f2192abc725e9670517766f55990caacf4
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-23 21:23:07 +0530

    nested if statement

---
+++ 013_Nested_If_Statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int a = 10;
+	int b = 20;
+	//Nested If Statement
+	if (a == 10){
+		if (b == 20){
+			printf("Yes a is 10 and b is 20 \n");
+		}
+	}
+	return 0;
+}
+
