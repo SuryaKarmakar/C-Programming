bed09c54c6ebda78c6b0e7998a0f59341747363e
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-02-05 22:43:04 +0530

    conditional operator

--- 020_Conditional_Operator.c
+++ 020_Conditional_Operator.c
@@ -16,10 +16,13 @@ int main(){
     y = x > z ? (x == 10 ? 1000 : 2000) : (z == 20 ? 3000 : 4000); 
     printf("%d\n",y); //here y will be print as 3000
 
-    // [example :4] use parenthess into right most statemet ,,when tyr to assign a value...
-    z > x ? (y = 11) : (y = 22);
+    /* [example :4] use parenthess into right most statemet ,,when tyr to assign a value... if you not use parentheses then the compiler give
+    you a error like 'Lvalue Required'. in absence of parentheses, the compiler believes that z is being assigned to the result of the expression
+    to the left of second =. Hence it reports an error... 
+    */
+    z > x ? y = x : (y = z);
     printf("%d\n",y);
-    
+
     return 0;
 }
 
