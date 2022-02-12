7d7ec7ef3e7baafea0d36eefd2dc19ecaba45798
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-02-12 23:50:21 +0530

    basic data types

---
+++ 003_Basic_Data_Types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // int -> As the name suggests, an int variable is used to store an integer.
+    int i = 10;
+    // float ->  It is used to store decimal numbers (numbers with floating point value) with single precision.
+    float f = 10.10;
+    // char -> The most basic data type in C. It stores a single character and requires a single byte of memory in almost all compilers.
+    char c = 'a';
+    // double -> It is used to store decimal numbers (numbers with floating point value) with double precision. 
+    double d = 3.14;
+    
+    return 0;
+}
