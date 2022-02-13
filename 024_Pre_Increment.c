9dbeda18c47811af294487d583894ab2e318a771
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-02-13 23:39:59 +0530

    pre increment

---
+++ 024_Pre_Increment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int j = 0, i = 1;
+	// pre increment -> first increments i, then assign the increment value i to j...
+	j = ++i;
+	// here j is now 2
+	printf("now j = %d\n",j);
+
+	return 0;
+}
