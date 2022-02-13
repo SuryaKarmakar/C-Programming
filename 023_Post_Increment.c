2ca91653a9777093513b45fd8dac4a65111452d9
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-02-13 23:36:17 +0530

    post increment

---
+++ 023_Post_Increment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int j = 0, i = 1;
+	// post increment -> first assign curent value of i to j, then i will be increment...
+	j = i++;
+	// here j is now 1
+	printf("now j = %d\n",j);
+
+	return 0;
+}
