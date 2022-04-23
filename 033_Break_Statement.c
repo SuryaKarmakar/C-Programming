9fcbb062e8c48ec66301eb00057eb933a43d4278
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-04-23 21:01:35 +0530

    break statement

--- 033_Break_Statement.c
+++ 033_Break_Statement.c
@@ -13,8 +13,8 @@ int main(){
 		
 	}
 	// ex 2:
-	/* the keyword break, breaks the control only form the while in witch it is placed. in this example when b equal to 5 then, break takes the control outside
-	the inner while only, since it is placed inside the inner while ...
+	/* the keyword break, breaks the control only form the while in witch it is placed. in this example when b equal to 5 then, break takes the 
+	control outside the inner while only, since it is placed inside the inner while ...
 	*/
 	int a = 1, b = 1;
 	while(a++ <= 10){
