9ae1804be2c72b017a00cee302c06ee7ec7cf14d
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-02-05 12:50:24 +0530

    with return and no argument

---
+++ 05_Functions/07_With_Return_No_Arguments.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//with return 
+int add(){
+	int x = 10, y = 20;
+    return (x + y);
+}
+
+int main(){
+    printf("i am main function...\n");
+    int sum = add();  //no arguments
+    printf("sum of a and b is : %d\n",sum);
+    return 0;
+}
