0ef1d20b9ea803d54c40ad6e83b33cb2fb966a6a
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-03-01 13:45:48 +0530

    print address of variable

---
+++ 06_Pointers/01_Print_Address_Of_Variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int i = 3;
+
+    // here we can use both "u%" or "d%" format specifier, "&" used for printing the address of a variable ...
+    printf("address of i : %u",&i);
+    return 0;
+}
