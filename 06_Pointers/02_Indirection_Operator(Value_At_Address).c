f5e2059a6a02a10b1beb41bf99dd711ad6c434bc
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-03-02 16:20:04 +0530

    indirection operator

--- 06_Pointers/02_Indirection_Operator(Value_At_Address).c
+++ 06_Pointers/02_Indirection_Operator(Value_At_Address).c
@@ -6,8 +6,8 @@ int main(){
 	printf("Address of i %u\n", &i); 
 	printf("Value of i %d \n", i);
 
-	/* '*' - this pointer operator is called 'value at address' operator, it gives the value stored at a particular address. The value at address operator is also called 'indirection
-	operator' */ 
+	/* '*' - this pointer operator is called 'value at address' operator, it gives the value stored at a particular address. The value at address 
+	operator is also called 'indirection operator' */ 
 	printf("value of i %d \n", *(&i)); // *(&i) -> value(address)
 	return 0;
 }
