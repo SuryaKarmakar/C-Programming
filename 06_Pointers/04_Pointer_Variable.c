6acd53b1687783244e8716539754089e8238aeeb
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-03-03 12:43:26 +0530

    pointer variable

---
+++ 06_Pointers/04_Pointer_Variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    /* here those 3 are declared as a pointer variable, that's means they can capable of holding address. The declaration float *f dose not mean
+    that 'f' is going to contain a floating-point value. What it means is 'f' is going to contain the address of a floating-point value... */
+    int *i;
+    char *ch;
+    float *f;
+
+    return 0;
+}
