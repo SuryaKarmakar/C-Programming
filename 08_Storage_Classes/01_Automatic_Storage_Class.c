d47c49cf6393fac2e502a64c2998c4272bb55546
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2023-05-06 14:44:52 +0530

    automatic storage class

--- 08_Storage_Classes/01_Automatic_Storage_Class.c
+++ 08_Storage_Classes/01_Automatic_Storage_Class.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 int mian (){
-	/*
-		storage - memory.
-		default value - an unpredictable garbage value.
-		scope - local to the block in which the variable is defiend.
-		life - till the control remains within the block in which the varible is define.
-	*/
-	auto int a;
-	printf("the garbage value of a is : %d\n",a);
+    /*
+        storage - memory.
+	default value - an unpredictable garbage value.
+	scope - local to the block in which the variable is defiend.
+	life - till the control remains within the block in which the varible is define.
+    */
+    auto int a;
+    printf("the garbage value of a is : %d\n",a);
 
-	auto int i = 1;
+    auto int i = 1;
+    {
+        auto int i = 2;
 	{
-		auto int i = 2;
-		{
-			auto int i = 3;
-			printf("%d\n",i);
-		}
-		printf("%d\n",i);
+	    auto int i = 3;
+	    printf("%d\n",i);
 	}
-	printf("%d\n",i);
-
-	return 0;
+        printf("%d\n",i);
+    }
+    printf("%d\n",i);
+    return 0;
 }
