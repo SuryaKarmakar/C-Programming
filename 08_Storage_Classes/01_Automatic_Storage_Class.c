c576ec913f0a8911c8f9f2da6fb699d6b3d629b3
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-05-06 14:35:13 +0530

    automatic storage class

---
+++ 08_Storage_Classes/01_Automatic_Storage_Class.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	/*
+		storage - memory.
+		default value - an unpredictable garbage value.
+		scope - local to the block in which the variable is defiend.
+		life - till the control remains within the block in which the varible is define.
+	*/
+	auto int a;
+	printf("the garbage value of a is : %d\n",a);
+
+	auto int i = 1;
+	{	
+		auto int i = 2;
+		{
+			auto int i = 3;
+			printf("%d\n",i);
+		}
+		printf("%d\n",i);
+	}
+	printf("%d\n",i);
+	
+	return 0;
+}
