b11724a647adb6e5dc1c45e3dda33e574fd6ed58
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-06-21 13:18:26 +0530

    external storage class

--- 08_Storage_Classes/04_External_Storage_Class.c
+++ 08_Storage_Classes/04_External_Storage_Class.c
@@ -5,11 +5,11 @@ extern int y = 20;
 
 void display(){
 	/*
-      storage - Memory(RAM).
-      default value - 0.
-      scope - global.
-      life - as long as the program's execution doesn't come to an end.
-   */
+		storage - Memory(RAM).
+		default value - 0.
+		scope - global.
+		life - as long as the program's execution doesn't come to an end.
+	*/
 	x += 1;
 	y -= 1;
 	printf("x = %d\n",x);
