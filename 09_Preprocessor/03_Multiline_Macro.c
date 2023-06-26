3b60af2d23911c736826f75def62038a6cef61f3
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2023-06-26 13:36:41 +0530

    multiline macro

--- 09_Preprocessor/03_Multiline_Macro.c
+++ 09_Preprocessor/03_Multiline_Macro.c
@@ -2,7 +2,7 @@
 
 // macro can be split multiple lines, with a '\' backslash present at the end of each line.
 #define ILOOP for(int i = 0; i <= 10; i++)\
-				printf("%d\n", i)
+		printf("%d\n", i)
 
 int main(){
 	ILOOP;
