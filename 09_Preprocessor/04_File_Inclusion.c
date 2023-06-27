c14d6dc2f9253331ceb06d71d088ea929cf70e4d
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-06-27 13:41:26 +0530

    file inclusion

---
+++ 09_Preprocessor/04_File_Inclusion.c
@@ -0,0 +1,16 @@
+// this <filename> command would look the specified list of directories(standard directory) only
+#include <stdio.h>
+
+/* this "filename" command would look for the file calculator.c in the current directory as well as the specified list of directories as mentioned in the include
+search path that might have been set up */
+#include "calculator.c"
+
+int main(){
+	int result;
+	result = sumOfTwoNumber(10, 5);
+	printf("sum of two number is %d\n", result);
+
+	result = subOfTwoNumber(10, 5);
+	printf("subtraction of two number is %d\n", result);
+	return 0;
+}
---
+++ 09_Preprocessor/calculator.c
@@ -0,0 +1,15 @@
+int sumOfTwoNumber(int a, int b){
+    return a + b;
+}
+
+int subOfTwoNumber(int a, int b){
+    return a - b;
+}
+
+int mulOfTwoNumber(int a, int b){
+    return a * b;
+}
+
+int divOfTwoNumber(int a, int b){
+    return a / b;
+}
