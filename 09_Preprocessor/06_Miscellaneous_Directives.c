0e78fdafb7345b3fcda3024275054a9d873d8500
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2023-07-11 13:08:55 +0530

    miscellaneous directive

---
+++ 09_Preprocessor/06_Miscellaneous_Directives.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define NAME
+// in order to undefine a macro that has been earlier #defined, we can use #undef dicertive.
+#undef NAME
+
+int main(){
+    #ifdef NAME
+        printf("NAME is #define as a macroname\n");
+    #endif
+
+    return 0;
+}
