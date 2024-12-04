70af646a50745dd25ee9174d7239333b4aaa7d25
SuryaKarmakar <suryakarmakar.development@gmail.com>
2024-12-04 16:21:08 +0530

    adding coment

--- 03-MCA-Class-Programming/01-Area-Of-Circle.c
+++ 03-MCA-Class-Programming/01-Area-Of-Circle.c
@@ -1,12 +1,16 @@
+/* Library file access */
 #include <stdio.h>
 
 int main() {
+    /*Variable declarations */
     float radius, area, pi = 3.14;
 
     printf("Redius : ");
+    /* Input statement */
     scanf("%f", &radius);
 
     area = pi * radius * radius;
+    /* Output statement */
     printf("Area = %f", area);
 
     return 0;
