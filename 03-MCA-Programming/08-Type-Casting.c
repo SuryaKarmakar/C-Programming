a47747da8df6e405c0825c5ef89d0ff8914c14c3
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-01-11 18:15:27 +0530

    type casting

---
+++ 03-MCA-Programming/08-Type-Casting.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("float to int : %d\n", (int)8.5);
+    printf("int to double : %f\n", (double)10);
+    printf("int to float : %f\n", (float)11);
+
+    return 0;
+}
