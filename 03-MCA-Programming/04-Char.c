d6210a8018ef1c918ff704998665ff93b0127244
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-01-11 18:15:05 +0530

    char in c

--- 03-MCA-Programming/04-Char.c
+++ 03-MCA-Programming/04-Char.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    // Character constants have integer values known as ASCII values.
-    printf("ASCII value of 'a' is %d \n", 'a');
-    printf("ASCII value of 97 is %c", 97);
+    // char are stored in 8 bits
+    char ch = 'a';
+    char ch5 = '5';
+    printf("ch = %c, ch5 = %c\n", ch, ch5);
+
+    // uing the format characters %d, the ASCII number of the character is displayed.
+    printf("ASCII value of 'a' is %d \n", ch);
+    printf("ASCII value of 97 is '%c'\n", 97);
 
     return 0;
 }
